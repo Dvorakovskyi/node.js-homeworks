1f18cb52a6582c1077a4f1701d69951bb21d4273
Volodymyr <vladimir.dvorakovskyi@gmail.com>
2023-10-30 19:00:59 +0200

    added CRM

--- app.js
+++ app.js
@@ -1,8 +1,8 @@
-
-import express from 'express';
-import logger from 'morgan';
-import cors from 'cors';
-import {contactsRouter, authRouter} from './routes/api/index.js';
+import express from "express";
+import logger from "morgan";
+import cors from "cors";
+import { contactsRouter, authRouter } from "./routes/api/index.js";
+import ordersRouter from "./routes/api/orders.js";
 
 const app = express();
 
@@ -11,10 +11,11 @@ const formatsLogger = app.get("env") === "development" ? "dev" : "short";
 app.use(logger(formatsLogger));
 app.use(cors());
 app.use(express.json());
-app.use(express.static('public'));
+app.use(express.static("public"));
 
 app.use("/api/users", authRouter);
 app.use("/api/contacts", contactsRouter);
+app.use("/api/orders", ordersRouter);
 
 app.use((req, res) => {
   res.status(404).json({ message: "Not found" });
